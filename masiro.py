9019776ad03bc86c4cc2f95ea1be01a9a5f36f90
Wu Junkai <wu.junkai@qq.com>
2023-03-19 22:45:34 +0800

    Update masiro.py

--- masiro.py
+++ masiro.py
@@ -1,10 +1,14 @@
 def analyze(data, method):
 	result = {}
 	for index in method['order']:
+		if(method[index]['return']=='return'):
+			return result[ method[index]['refer'] ]
 		if(method[index]['type']=="regex"):
 			result[index] = re.search(method[index]["rule"], data).group()
 		elif(method[index]['type']=='find'):
 			result[index] = re.findall(method[index]["rule"], data)
+		elif(method[index]["type"]=='func'):
+			pass
 		elif(method[index]['type']=='range'):
 			result[index] = []
 			for sub in result[ method[index]['refer'] ]:
