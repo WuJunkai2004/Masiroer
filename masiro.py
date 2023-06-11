62d1f8b60fd72bdfae14a7c9d88bee04f3f73e8f
Wu Junkai <wu.junkai@qq.com>
2023-06-11 17:03:08 +0800

    add login

--- masiro.py
+++ masiro.py
@@ -4,23 +4,30 @@ import requests
 
 import sys
 
-class cookies:
+class _cookies:
     __cookie__ = {}
     def __init__(self):
         pass
-    def __setattr__(self, name, value):
+    def __setitem__(self, name, value):
         self.__cookie__[name] = value
-    def __getattr__(self, name):
+    def __getitem__(self, name):
         return self.__cookie__[name]
     def get_all(self):
         return self.__cookie__
+    def save(self, gets):
+        for name in gets.keys():
+            self[name] = gets[name]
+
+class defined_cookies(_cookies):
+    pass
 
 
 class executor:
-    def __init__(self, config_file):
+    def __init__(self, config_file, use_cookie = defined_cookies):
         self.is_load = False
         self.file = config_file
-        self.rule = ''
+        self.rule = None
+        self.cook = use_cookie
 
     def load(self):
         try:
@@ -36,10 +43,29 @@ class executor:
     def __call__(self, *args, **kwargs):
         if(not self.is_load):
             self.load()
+        print(self.rule)
         data = self.translate_variables(self.rule['data']['dataset'], args, kwargs)
-        base, info = self.translate_request()
-        text = requests.request(*base, **info).text
-        return self.translate_content(text, self.rule['response'])
+        base, info = self.translate_request( data )
+
+        request = requests.request(*base, **info)
+
+        print(request.text)
+
+        self.cook().save(request.cookies.get_dict())
+        return self.translate_content(request.text, self.rule['response'])
+    
+    def translate_request(self, data):
+        base = ["POST"if(self.rule['isWithData'])else'GET', self.rule['URL'] ]
+        info = {
+            'headers' : {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+        }
+        if(self.rule['isWithData']):
+            data_type = {"urlencoded":"data",
+                         }[self.rule['data']['method']]
+            info[data_type] = data
+        if(self.rule['isWithCookie']):
+            info['cookies'] = self.cook().get_all()
+        return base, info
 
     def translate_variables(self, dataset, args, kwargs):
         result = {}
@@ -47,14 +73,15 @@ class executor:
             result[ dataset[index]['name'] ] = args[index]
         result.update(kwargs)
         for item in dataset:
-            pass#验证是否含有全部必须变量
+            if(item['required'] and item['name'] not in result.keys()):
+                raise AttributeError('The Attibution of {} is Required'.format(item['name']))
         return result
 
     def translate_content(self, data, method):
         result = {}
         for index in method['order']:
             if(method[index]['type']=='return'):
-                 return result[ method[index]['refer'] ]
+                return result[ method[index]['refer'] ]
             if(method[index]['type']=="regex"):
                 result[index] = re.search(method[index]["rule"], data).group()
             elif(method[index]['type']=='find'):
@@ -62,11 +89,10 @@ class executor:
             elif(method[index]["type"]=='func'):
                 if(method[index]["func"]=='json'):
                     result = json.loads(data)
-                pass #若有其他需求
             elif(method[index]['type']=='range'):
                 result[index] = []
                 for sub in result[ method[index]['refer'] ]:
-	                result[index].append( self.translate_content(sub, method[index]['rule']) )
+                    result[index].append( self.translate_content(sub, method[index]['rule']) )
             else:
                 raise RuntimeError('unknown type: {}'.format(index))
         return result
@@ -75,14 +101,14 @@ class executor:
 class categories:
     def __init__(self, load):
         for files in load:
-            self.setattr(files['name'], executor('instruct\\' + files['path']))
+            setattr(self, files['name'], executor('configure\\' + files['file']))
 
 
 with open('config.json', 'r') as fin:
     _config = json.load(fin)
 
-forum = categories( _config['forum'] )
-users = categories( _config['users'] )
-books = categories( _config['books'] )
-lists = categories( _config['lists'] )
+forum   = categories( _config['forum'] )
+users   = categories( _config['users'] )
+books   = categories( _config['books'] )
+lists   = categories( _config['lists'] )
 myself  = categories( _config['myself'] )
