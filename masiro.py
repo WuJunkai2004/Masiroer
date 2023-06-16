c7fc8722901125a06ca9fbca9874e6688dea6097
Wu Junkai <wu.junkai@qq.com>
2023-06-16 22:22:42 +0800

    6/16 update
    - 添加了新的解析方法
    - 优化了cookie的处理方法
    - 初步添加了search和collection

--- masiro.py
+++ masiro.py
@@ -7,21 +7,37 @@ import requests as _requests
 
 class _cookies:
     __cookie__ = {}
+    def __init__(self, file = None):
+        if(file):
+            self.start(file)
+
     def __setitem__(self, name, value):
         self.__cookie__[name] = value
+        self.__handle__.write( '{}\t{}\n'.format(name, value) )
+
     def __getitem__(self, name):
         return self.__cookie__[name]
+    
+    def __del__(self):
+        if( hasattr(self, '__handle__') ):
+            self.__handle__.close()
+    
+    def start(self, file):
+        with open(file, 'r') as fin:
+            for line in fin.readlines():
+                self.__cookie__.__setitem__( *line[:-1].split('\t', 1) )
+        self.__handle__ = open(file, 'a+')
+    
     def gets(self):
         return self.__cookie__
-    def puts(self, gets):
-        for name in gets.keys():
-            self.__cookie__[name] = gets[name]
-    def load(self, file):
-        with open(file, 'r') as fin:
-            self.puts( _json.load(fin) )
-    def dump(self, file):
-        with open(file, 'w+') as fout:
-            _json.dump(self.__cookie__, fout)
+
+    def puts(self, get):
+        for name in get.keys():
+            self.__cookie__[name] = get[name]
+
+    def clean(self):
+        pass
+
 
 class defined_cookies(_cookies):
     pass
@@ -41,7 +57,7 @@ class _executor:
         except FileNotFoundError:
             print("Error: File not found")
         except _json.JSONDecodeError:
-            print('Error: Invalid _json data in file')
+            print('Error: Invalid json data in file')
         else:
             self.is_load = True
 
@@ -53,12 +69,17 @@ class _executor:
 
         request = _requests.request(*base, **info)
 
-        print(request.text)
-
         if(self.rule['isSaveCookie']):
-            self.cook().puts(request.cookies.get_dict())
+            self.cook().puts( request.cookies.get_dict() )
 
-        return self.translate_content(request.text, self.rule['response'])
+        try:
+            result = self.translate_content(request.text, self.rule['response'])
+        except:
+            _sys.stderr.write("Error: translatee content")
+            _sys.stdout.write(request.text)
+        else:
+            return result
+        
     
     def translate_request(self, data):
         base = ["POST"if(self.rule['isWithData'] and self.rule['data']['method'] not in ('params'))else'GET', self.rule['URL'] ]
@@ -69,7 +90,7 @@ class _executor:
                 'Accept-Language':'zh-CN,zh;q=0.9,en;q=0.8'
             }
         }
-        print(base,file = _sys.stderr)
+        _sys.stderr.write( str(base) )
         if(self.rule['isWithData']):
             info[ self.rule['data']['method'] ] = data
         if(self.rule['isWithCookie']):
@@ -89,16 +110,26 @@ class _executor:
     def translate_content(self, data, method):
         result = {}
         for index in method['order']:
-            if(method[index]['type']=='return'):
+            if  ( method[index]['type'] == 'return' ):
                 return result[ method[index]['refer'] ]
-            if(method[index]['type']=="regex"):
+            if  ( method[index]['type'] == "regex"  ):
                 result[index] = _re.search(method[index]["rule"], data).group()
-            elif(method[index]['type']=='find'):
+            elif( method[index]['type'] == 'find'   ):
                 result[index] = _re.findall(method[index]["rule"], data)
-            elif(method[index]["type"]=='func'):
-                if(method[index]["func"]=='_json'):
+            elif( method[index]["type"] == 'func'   ):
+                if  ( method[index]["func"] == 'json' ):
                     result[index] = _json.loads(data)
-            elif(method[index]['type']=='range'):
+                elif( method[index]["func"] == "code" ):
+                    data = data.encode().decode("unicode_escape")
+                elif( method[index]["func"] == "save" ):
+                    with open("temp.txt","w+") as fout:
+                        fout.write(data)
+            elif( method[index]['type'] == 'refer'  ):
+                get = result
+                for item in method[index]["rule"]:
+                    get = get[item]
+                result[index] = get
+            elif( method[index]['type'] ==' range'  ):
                 result[index] = []
                 for sub in result[ method[index]['refer'] ]:
                     result[index].append( self.translate_content(sub, method[index]['rule']) )
@@ -114,11 +145,11 @@ class _categories:
 
 
 def _load_config(file):
-    with open('config._json', 'r') as fin:
+    with open('config.json', 'r') as fin:
         return _json.load(fin)
 
 
-_config = _load_config('config._json')
+_config = _load_config('config.json')
 
 forum = _categories( _config['forum'] )
 lists = _categories( _config['lists'] )
